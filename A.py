e3da0feeda835d4e2a3605d1842c66b7f7753b0d
rafamancin1 <50625660+rafamancin1@users.noreply.github.com>
2019-07-01 10:53:43 -0300

    Add files via upload

---
+++ A.py
@@ -0,0 +1,7 @@
+class A():
+  A1 = 0
+  A2 = 0.0
+  def MA1(self):
+    print("MA1")
+  def MA2(self):
+    print("MA2")
---
+++ B.py
@@ -0,0 +1,7 @@
+class B():
+  B1 = 0
+  B2 = 0.0
+  def MB1(self):
+    print("MB1")
+  def MB2(self):
+    print("MB2")
